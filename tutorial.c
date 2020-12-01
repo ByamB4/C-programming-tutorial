aa9cf28057f01f3c92fb114705ea89f096214823
ByamB4 <byamba4life@gmail.com>
2020-12-01 09:59:24 +0800

    Хичээл - 3

--- tutorial.c
+++ tutorial.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-/*
-    Энэхүү код дэлгэцэнд Hello World гэж
-    хэвлэх бөгөөд ирээдүйд маш хэрэгтэй 
-    код болох тул битгий устгаарай. 
-*/
-
 int main() {
-    printf("Bi shine mortoi hevlene\n");
-    printf("Bi shine morgvi hevlene");
-    printf("\tTab zai avch baina");
+    int number_a = 5;
+    int number_b = 10;
+    printf("Sum of numbers: %d", number_a + number_b);
+
+    // int              : -2,147,483,648 to 2,147,483,647
+    // unsigned int     : 0 to 65,535 or 0 to 4,294,967,295
+    // short            : -32,768 to 32,767
+    // unsigned short   : 0 to 65,535
+    // long             : -9223372036854775808 to 9223372036854775807
+    // unsigned long    : 0 to 18446744073709551615
 }
